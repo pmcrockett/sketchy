03faf44582baa0169af36b7d676900f5cec97dc1
pmcrockett <102689325+pmcrockett@users.noreply.github.com>
2022-12-19 02:14:20 -0800

    Detect mobile and improve undo performance

--- sketchy.js
+++ sketchy.js
@@ -22,7 +22,8 @@ let gifButton = document.querySelector(".gif-button");
 let gridButton = document.querySelector(".grid-button");
 let undoButton = document.querySelector(".undo-button");
 let redoButton = document.querySelector(".redo-button");
-let buttons = [resButton, animButton, pngButton, gifButton, gridButton];
+let buttons = [resButton, animButton, pngButton, gifButton, gridButton, 
+        undoButton, redoButton];
 let drawCtx;
 let basisCtx;
 let overlayCtx;
@@ -30,7 +31,6 @@ let grid;
 let animBasis;
 let bufferCanvas;
 let resolution;
-let clickHeld = false;
 let fillCalls;
 let lastPixelPos = {};
 let animFrames = [];
@@ -39,14 +39,18 @@ let animTimer;
 let isExporting = false;
 let exportingSquare;
 let lineCooldown = false;
-let fillPixels = [];
+let pixelBuffer = [];
 let redoStack = [];
+let isMobile = false;
 
 class StateManager {
     constructor() {
         this.isAnimating = false;
         this._isExporting = false;
         this._isFilling = false;
+        this.clickHeld = false;
+        this.isDrawingLine = false;
+        this.isDrawingPoint = false;
     }
 
     isBlockingInput() {
@@ -94,7 +98,7 @@ function enablebuttons() {
     }
 }
 
-function init(_fieldSize, _resolution, _color) {
+function initCanvas(_fieldSize, _resolution, _color) {
     resolution = _resolution;
     if (stateManager.isAnimating) toggleAnimating();
     drawField.width = resolution;
@@ -115,6 +119,14 @@ function init(_fieldSize, _resolution, _color) {
     animFrames = [{ pixel: [], type: "init" }];
 }
 
+function getPageX(_e) {
+    return _e.pageX || _e.changedTouches[0].pageX;
+}
+
+function getPageY(_e) {
+    return _e.pageY || _e.changedTouches[0].pageY;
+}
+
 function getPixelPosition(_pageX, _pageY) {
     //console.log(`x: ${_e.pageX}, y: ${_e.pageY}`);
     let xPosInCanvas = _pageX - drawField.offsetLeft;
@@ -127,21 +139,28 @@ function getPixelPosition(_pageX, _pageY) {
 }
 
 function clickDraw(_e) {
+    _e.preventDefault();
+    //console.log(_e);
     if (stateManager.isBlockingInput()) return;
     _e.preventDefault();
     if (!stateManager.isAnimating) {
-        if (_e.button === 0) {
-            clickHeld = true;
+        if (_e.type === "dblclick" && _e.button === 0) {
+            undo();
+            // fillCalls = 1;
+            // fillPixels = [];
+            // let pixelPos = getPixelPosition(_e.pageX, _e.pageY);
+            // let oldColorArr = drawCtx.getImageData(pixelPos.x, pixelPos.y, 
+            //         1, 1).data;
+            // let oldColor = getRGBAFromArray(oldColorArr, true);
+            // fill(pixelPos, oldColor, "rgba(0, 0, 0, 1)");
+            // redoStack = [];
+        } else if (_e.button === 0 || _e.changedTouches) {
+            stateManager.clickHeld = true;
+            stateManager.isDrawingPoint = true;
             drawPoint(_e, "rgba(0, 0, 0, 1)");
             redoStack = [];
         } else if (_e.button === 2) {
-            fillCalls = 1;
-            fillPixels = [];
-            let pixelPos = getPixelPosition(_e.pageX, _e.pageY);
-            let oldColorArr = drawCtx.getImageData(pixelPos.x, pixelPos.y, 1, 1).data;
-            let oldColor = getRGBAFromArray(oldColorArr, true);
-            fill(pixelPos, oldColor, "rgba(0, 0, 0, 1)");
-            redoStack = [];
+
         }
     }
 }
@@ -149,9 +168,12 @@ function clickDraw(_e) {
 function dragDraw(_e) {
     //console.log(`drag draw enter`);
     if (stateManager.isBlockingInput()) return;
-    if (clickHeld && !stateManager.isAnimating && 
-        (animFrames[animFrames.length - 1].type === "drawLine" || getPixelPosition() != lastPixelPos)) {
-        console.log(animFrames[animFrames.length - 1].type);
+    if (stateManager.clickHeld && !stateManager.isAnimating && 
+        (animFrames[animFrames.length - 1].type === "drawLine" || 
+            getPixelPosition() != lastPixelPos)) {
+        //console.log(animFrames[animFrames.length - 1].type);
+        stateManager.isDrawingPoint = false;
+        stateManager.isDrawingLine = true;
         drawLine(lastPixelPos, _e, "rgba(0, 0, 0, 1)");
         redoStack = [];
         //console.log(`drag draw`);
@@ -160,29 +182,36 @@ function dragDraw(_e) {
 }
 
 function drawPoint(_e, _color) {
-    let pixelPos = getPixelPosition(_e.pageX, _e.pageY);
+    let pixelPos = getPixelPosition(getPageX(_e), getPageY(_e));
+    let oldColor = getRGBAFromPoint(pixelPos, drawCtx);
     drawCtx.fillStyle = _color;
     drawCtx.fillRect(pixelPos.x, pixelPos.y, 1, 1);
     if (gridButton.classList.contains("depressed")) updateGridPoint(pixelPos);
     lastPixelPos = pixelPos;
-    animFrames.push({ pixel: [{ position: pixelPos, color: _color }], type: "drawPoint" });
+    pixelBuffer.push({ position: pixelPos, color: _color, oldColor: oldColor });
 }
 
 function drawAnimPoint(_pixel, _context, _updateGrid = true) {
     _context.fillStyle = _pixel.color;
     _context.fillRect(_pixel.position.x, _pixel.position.y, 1, 1);
-    if (_updateGrid && gridButton.classList.contains("depressed")) updateGridPoint(_pixel.position);
+    if (_updateGrid && gridButton.classList.contains("depressed")) {
+        updateGridPoint(_pixel.position);
+    }
 }
 
 /* This function is necessary to fix a problem where lag causes skips in what
 should be a continuously drawn line. We're playing connect-the-dots between 
 known cursor positions, basically. */
 function drawLine(_oldPixelPos, _e, _color) {
-    /* drawLine will always be preceded by drawPoint because we can't know the 
+    /* drawLine may be preceded by drawPoint because we can't know the 
     user's intent initially. If they go on to draw a line, we move the animFrame 
     that we drew for the initial point into the line's animFrame. */
-    let pixels = animFrames.pop().pixel;
-    let pixelPos = getPixelPosition(_e.pageX, _e.pageY);
+    /*
+    if (animFrames[animFrames.length - 1].type === "drawPoint") {
+        pixelBuffer.push(animFrames.pop().pixel);
+    }
+    */
+    let pixelPos = getPixelPosition(getPageX(_e), getPageY(_e));
     drawCtx.fillStyle = _color;
     let xDist = Math.abs(pixelPos.x - _oldPixelPos.x);
     let yDist = Math.abs(pixelPos.y - _oldPixelPos.y);
@@ -199,18 +228,19 @@ function drawLine(_oldPixelPos, _e, _color) {
         else linePos.x = move.left;
         if (_oldPixelPos.y < pixelPos.y) linePos.y = move.down;
         else linePos.y = move.up;
+        let oldColor = getRGBAFromPoint(linePos, drawCtx);
         drawCtx.fillRect(linePos.x, linePos.y, 1, 1);
         if (gridButton.classList.contains("depressed")) updateGridPoint(linePos);
-        pixels.push({ position: linePos, color: _color });
+        pixelBuffer.push({ position: linePos, color: _color, oldColor: oldColor });
     }
     lastPixelPos = pixelPos;
-    animFrames.push({ pixel: pixels, type: "drawLine" });
+    //animFrames.push({ pixel: pixels, type: "drawLine" });
 }
 
 function fill(_pixelPos, _oldColor, _newColor) {
     //console.log(`entering fill (calls: ${fillCalls})`);
     if (_oldColor === _newColor) {
-        fillPixels = [{ position: _pixelPos, color: _newColor }];
+        pixelBuffer = [{ position: _pixelPos, color: _newColor, oldColor: _oldColor }];
         closeFillCall();
         return;
     }
@@ -235,7 +265,8 @@ function fill(_pixelPos, _oldColor, _newColor) {
     function closeFillCall() {
         fillCalls--;
         if (fillCalls <= 0) {
-            animFrames.push({ pixel: fillPixels, type: "fill" });
+            animFrames.push({ pixel: pixelBuffer, type: "fill" });
+            pixelBuffer = [];
             stateManager.isFilling = false;
         }
         //console.log(`${stateManager.isBlockingInput()}, ${fillCalls} calls`);
@@ -289,7 +320,7 @@ function fill(_pixelPos, _oldColor, _newColor) {
         }
     }
     closeFillCall();
-    fillPixels.push({ position: _pixelPos, color: _newColor });
+    pixelBuffer.push({ position: _pixelPos, color: _newColor, oldColor: _oldColor });
 }
 
 function readKey(_e) {
@@ -299,15 +330,17 @@ function readKey(_e) {
 
 function setRes() {
     if (resInput.value === "") {
-        init(getViewableSize(), parseInt(resInput.getAttribute("placeholder")), "rgba(255, 255, 255, 1)");
+        initCanvas(getViewableSize(), parseInt(resInput.getAttribute("placeholder")), 
+                "rgba(255, 255, 255, 1)");
         resInput.setAttribute("placeholder", `${DEFAULT_RES}`);
     }
-    else if (!parseInt(resInput.value) || parseInt(resInput.value) > 128 || parseInt(resInput.value) < 1) {
+    else if (!parseInt(resInput.value) || parseInt(resInput.value) > 128 || 
+        parseInt(resInput.value) < 1) {
         resInput.value = "";
         resInput.setAttribute("placeholder", "Enter a number between 1 and 128");
     }
     else {
-        init(getViewableSize(), parseInt(resInput.value), "rgba(255, 255, 255, 1)");
+        initCanvas(getViewableSize(), parseInt(resInput.value), "rgba(255, 255, 255, 1)");
         resInput.setAttribute("placeholder", `${DEFAULT_RES}`);
     }
 }
@@ -331,7 +364,8 @@ function toggleAnimating() {
 
 function drawFrame(_frameNum, _context, _drawAllSubframes = false, _updateGrid = true) {
     function drawSubframes(_subframeCount) {
-        let loopLimit = Math.min(animFrames[nextFrame].pixel.length - 1, nextSubframe + _subframeCount);
+        let loopLimit = Math.min(animFrames[nextFrame].pixel.length - 1, 
+                nextSubframe + _subframeCount);
         for (let i = nextSubframe; i <= loopLimit; i++) {
             drawAnimPoint(animFrames[nextFrame].pixel[i], _context, _updateGrid);
             if (i + 1 < animFrames[nextFrame].pixel.length) {
@@ -443,6 +477,10 @@ function getRGBAFromArray(_arr, _alphaIs255 = false) {
     } else return `rgba(${_arr[0]}, ${_arr[1]}, ${_arr[2]}, ${_arr[3]})`;
 }
 
+function getRGBAFromPoint(_pixelPos, _context) {
+    return getRGBAFromArray(_context.getImageData(_pixelPos.x, _pixelPos.y, 1, 1).data, true);
+}
+
 function startExport(_msg) {
     stateManager.isExporting = true;
     //bufferCanvas = new OffscreenCanvas(resolution, resolution);
@@ -532,7 +570,8 @@ function exportGif() {
 }
 
 function setOverlayPosition() {
-    overlayCanvas.style.transform = `translate(0, ${menuBar.clientHeight - window.scrollY}px)`;
+    overlayCanvas.style.transform = `translate(0, 
+            ${menuBar.clientHeight - window.scrollY}px)`;
 }
 
 function toggleGrid() {
@@ -552,17 +591,8 @@ function showGrid() {
         for (let y = 0; y <= resolution; y++) {
             updateGridPoint({ x: x, y: y });
         }
-        //let drawPos = Math.round((viewable - 1) * (x / resolution));
-        //overlayCtx.fillRect(drawPos, 0, 1, viewable);
     }
-    /*
-    for (let y = 0; y <= resolution; y++) {
-        let drawPos = Math.round((viewable - 1) * (y / resolution));
-        overlayCtx.fillRect(0, drawPos, viewable, 1);
-    }
-    */
 }
-
 
 function hideGrid() {
     let viewable = getViewableSize();
@@ -582,11 +612,13 @@ function updateGridPoint(_pixelPos) {
     }
     pixDat[3] = 1;
     overlayCtx.fillStyle = getRGBAFromArray(pixDat);
-    overlayCtx.fillRect(drawPosX, drawPosY, 1, increment);
-    overlayCtx.fillRect(drawPosX, drawPosY, increment, 1);
+    let lineWidth = isMobile ? 2 : 1;
+    overlayCtx.fillRect(drawPosX, drawPosY, lineWidth, increment);
+    overlayCtx.fillRect(drawPosX, drawPosY, increment, lineWidth);
 }
 
 function undo() {
+    /*
     if (animFrames.length > 1) {
         redoStack.push(animFrames.pop());
         console.log(animFrames.length);
@@ -595,6 +627,21 @@ function undo() {
             nextFrame = drawFrame(nextFrame, drawCtx, true, false);
         } while (nextFrame[0] > 0 && animFrames.length > 1);
         if (gridButton.classList.contains("depressed")) showGrid();
+    }
+    */
+    if (animFrames.length > 1) {
+        //for (let pixel of animFrames[animFrames.length - 1].pixel) {
+        for (i = animFrames[animFrames.length - 1].pixel.length - 1; i >= 0; i--) {
+            let pixel = animFrames[animFrames.length - 1].pixel[i];
+            console.log(`undoing`);
+            drawCtx.fillStyle = pixel.oldColor;
+            console.log(`oldColor: ${pixel.oldColor}`);
+            drawCtx.fillRect(pixel.position.x, pixel.position.y, 1, 1);
+            if (gridButton.classList.contains("depressed")) {
+                updateGridPoint(pixel.position);
+            }
+        }
+        redoStack.push(animFrames.pop());
     }
 }
 
@@ -605,31 +652,76 @@ function redo() {
     }
 }
 
+function endDraw(_e) {
+    if (_e.button === 0) {
+        stateManager.clickHeld = false;
+        if (stateManager.isDrawingPoint) {
+            stateManager.isDrawingPoint = false;
+            animFrames.push({ pixel: pixelBuffer, type: "drawPoint" });
+            pixelBuffer = [];
+        } else if (stateManager.isDrawingLine) {
+            stateManager.isDrawingLine = false;
+            animFrames.push({ pixel: pixelBuffer, type: "drawLine" });
+            pixelBuffer = [];
+        }
+    }
+    console.log(animFrames);
+}
+
+function checkIfMobile() {
+    /* Mobile detection RegEx strings from 
+    https://phppot.com/javascript/detect-mobile-device-javascript/ */
+    return /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i
+    .test(navigator.userAgent)
+    || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i
+            .test(navigator.userAgent.substring(0, 4));
+}
+
+function initMobile() {
+    for (let elem of document.getElementsByTagName("*")) {
+        elem.classList.add("mobile");
+    }
+    isMobile = true;
+}
+
+function initListeners() {
+    // Input devices
+    resInput.addEventListener("keydown", readKey);
+    document.addEventListener("mouseup", endDraw);
+    document.addEventListener("touchend", endDraw);
+    overlayCanvas.addEventListener("mousedown", clickDraw);
+    overlayCanvas.addEventListener("dblclick", clickDraw);
+    overlayCanvas.addEventListener("touchstart", clickDraw);
+    overlayCanvas.addEventListener("mousemove", dragDraw);
+    overlayCanvas.addEventListener("touchmove", dragDraw);
+    // Buttons
+    animButton.addEventListener("click", toggleAnimating);
+    gridButton.addEventListener("click", toggleGrid);
+    resButton.addEventListener("click", setRes);
+    pngButton.addEventListener("click", exportPng);
+    gifButton.addEventListener("click", exportGif);
+    undoButton.addEventListener("click", undo);
+    redoButton.addEventListener("click", redo);
+    // Window actions
+    document.addEventListener("contextmenu", (_e) => {
+        _e.preventDefault();
+    });
+    window.addEventListener("resize", (_e) => {
+        setDrawFieldSize(getViewableSize());
+    });
+    window.addEventListener("scroll", setOverlayPosition);
+}
+
+// Program execution
+const stateManager = new StateManager();
+if (checkIfMobile()) initMobile();
+initCanvas(getViewableSize(), parseInt(resInput.getAttribute("placeholder")), 
+    "rgba(255, 255, 255, 1)");
+initListeners();
+
+// Debug
 console.log(`r: ${getRfromRGBA("rgba(0, 1,2,0.5)")}`);
 console.log(`g: ${getGfromRGBA("rgb(0,1,2,0.5)")}`);
 console.log(`b: ${getBfromRGBA("rgba(0,1,2,0.5)")}`);
 console.log(`a: ${getAfromRGBA("rgb( 0, 5,2)")}`);
-const stateManager = new StateManager();
 console.log(stateManager.isBlockingInput());
-init(getViewableSize(), parseInt(resInput.getAttribute("placeholder")), "rgba(255, 255, 255, 1)");
-resButton.addEventListener("click", setRes);
-resInput.addEventListener("keydown", readKey);
-document.addEventListener("mouseup", (_e) => {
-    if (_e.button === 0) clickHeld = false;
-    console.log(animFrames);
-});
-overlayCanvas.addEventListener("mousedown", clickDraw);
-overlayCanvas.addEventListener("mousemove", dragDraw);
-document.addEventListener("contextmenu", (_e) => {
-    _e.preventDefault();
-});
-animButton.addEventListener("click", toggleAnimating);
-gridButton.addEventListener("click", toggleGrid);
-window.addEventListener("resize", (_e) => {
-    setDrawFieldSize(getViewableSize());
-});
-window.addEventListener("scroll", setOverlayPosition);
-pngButton.addEventListener("click", exportPng);
-gifButton.addEventListener("click", exportGif);
-undoButton.addEventListener("click", undo);
-redoButton.addEventListener("click", redo);
